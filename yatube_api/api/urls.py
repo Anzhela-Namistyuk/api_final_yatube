711b78daf6e25191860dfddb070a164cc60c45c5
Анжела <anzhela.namistyuk@yandex.ru>
2021-12-02 11:17:57 +0300

    1st commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register('follow', FollowViewSet)
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet,
+                basename='comments')
 
 urlpatterns = [
+    path(r'v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('v1/', include('djoser.urls.jwt')),
 ]
